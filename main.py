d51a51e737da4cb2c77a195d2fb4fd196b86f8b7
IDGS-802-23002172 <83305@alumnos.utleon.edu.mx>
2026-01-08 18:53:34 -0600

    Templates

--- main.py
+++ main.py
@@ -1,12 +1,64 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
+def index():
+    titulo="IDGS-802-Flask"
+    lista= ['Juan', 'Karla', 'Miguel', 'Ana']
+    return render_template('index.html',titulo=titulo,lista=lista)
 
-def home():
 
-    return "Hello, world!"
+@app.route('/formularios')
+def formularios():
+    return render_template("formularios.html")
+
+@app.route('/reportes')
+def reportes():
+    return render_template("reportes.html")
+
+
+
+@app.route('/hola')
+def hola():
+    return "¡Hola, hola!"
+
+@app.route('/user/<user>')
+def user(user):
+    return f"¡Hola, {user}!"
+
+@app.route('/numero/<int:n>')
+def numero(n):
+    return "Numero: {}" .format(n)
+
+@app.route('/user/<int:id>/<string:username>')
+def username(id, username):
+    return "ID: {} nombre: {}" .format(id, username)
+
+@app.route('/suma/<float:n1>/<float:n2>')
+def func(n1, n2):
+    return "la suma es: {}" .format(n1+n2)
+    
+    
+@app.route("/default")
+@app.route("/default/<string:param>")
+def func2(param="juan"):
+    return f"<h1>!Hola, {param}!</h1>"
+
+@app.route("/operas")
+def operas():
+    return '''
+    <form>
+    <label for "name">Nombre:</label>
+    <input type="text" id="name" name="name" required>
+    
+    <label for "name">apaterno:</label>
+    <input type="text" id="name" name="name" required>
+    </form>
+
+'''
+
+
 
 if __name__  == '__main__':
 
